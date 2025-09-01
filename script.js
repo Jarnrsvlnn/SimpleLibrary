1fa2ee54433dfd10270c4e0f3c10be5425c083f4
Jarnrsvlnn <173128640+jarnrsvlnn@users.noreply.github.com>
2025-09-01 18:32:48 +0800

    Added some functions such as to add book, display book, and a button that displays a form when clicked

---
+++ script.js
@@ -0,0 +1,45 @@
+const container = document.querySelector(".container");
+const newBook = document.querySelector(".new-book");
+
+const myLibrary = [];
+
+function Book(title, author, pages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+    this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, hasRead) {
+    const book = new Book(title, author, pages, hasRead);
+    myLibrary.push(book);
+}
+
+function displayBooks() {
+    container.innerHTML = "";
+
+    for (const book of myLibrary) {
+        const bookCard = document.createElement("div");
+        const booktitle = document.createElement("p");
+        const bookAuthor = document.createElement("p");
+        const bookPages = document.createElement("p");
+        const bookReadState = document.createElement("p");
+        
+        booktitle.textContent = `${book.title}`;
+        bookAuthor.textContent = `${book.author}`;
+        bookPages.textContent = `${book.pages}`;
+        bookReadState.textContent = `${book.hasRead}`;
+
+        bookCard.append(booktitle);
+        bookCard.append(bookAuthor);
+        bookCard.append(bookPages);
+        bookCard.append(bookReadState);
+
+        container.append(bookCard);
+    }
+}
+
+newBook.addEventListener("click", () => {
+    
+})
